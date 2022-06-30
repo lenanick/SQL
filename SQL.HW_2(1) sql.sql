f3f6d87632005796325bba2595389ff591e19838
Lena Tsiuper <99662807+lenanick@users.noreply.github.com>
2022-06-30 20:48:55 +0300

    Update SQL.HW_2(1) sql.sql

--- SQL.HW_2(1) sql.sql
+++ SQL.HW_2(1) sql.sql
@@ -78,6 +78,7 @@ values ('Xavia Edwards'),
        ('Lana Diaz'),
        ('Olida Ramirez');
        
+       
 -- Создать таблицу salary (id. Serial  primary key, monthly_salary. Int, not null). 
 -- Наполнить таблицу salary 15 строками:
            
@@ -101,12 +102,9 @@ values (1000),
        (2100),
        (2200),
        (2300),
-       (2400),
-       (2500),
-       (2600),
-       (2700),
-       (2800),
-       (2900); 
+       (2400);
+       
+    
        
 -- Создать таблицу employee_salary (id. Serial  primary key, employee_id. Int, not null, unique; salary_id. Int, not null)
 -- Наполнить таблицу employee_salary 40 строками: в 10 строк из 40 вставить несуществующие employee_id
@@ -161,6 +159,7 @@ values (1, 1),
        (79, 20),
        (80, 1);
        
+       
 -- Создать таблицу roles (id. Serial  primary key, role_name. int, not null, unique)
 -- Поменять тип столба role_name с int на varchar(30)
 -- Наполнить таблицу roles 20 строками:
@@ -194,6 +193,7 @@ values  ('Junior Python developer'),
         ('Junior Automation QA engineer'),
         ('Middle Automation QA engineer'),
         ('Senior Automation QA engineer');    
+        
         
 -- Создать таблицу roles_employee (id. Serial  primary key, employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id) role_id. Int, not null (внешний ключ для таблицы roles, поле id)
 -- Наполнить таблицу roles_employee 40 строками:
